b52931a4ca4d01e75d166aab98bb313bbc4d823a
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-05-06 22:05:32 +0200

    DOMAIN: Prepare 'Create domain' card

--- public/js/domain.js
+++ public/js/domain.js
@@ -2,6 +2,22 @@
 /********************************************* Chargement du synoptique 1 au démrrage *****************************************/
  function Load_page ()
   { console.log ("in load domain !");
+
+    let card = $("<div id='idpluscard'>").addClass("card shadow m-1 bg-light");
+    let header = $("<h4>").text( "Nouveau" );
+    card.append ( $("<div>").addClass("card-header text-center").append(header) );
+
+    let body = $("<img>").css("cursor","pointer").addClass("wtd-img-card")
+               .click( function () { Creer_domaine ( element.domain_uuid ); } )
+               .attr ("src", "https://static.abls-habitat.fr/img/plus_green.svg" );
+    card.append ( $("<div>").addClass("card-body text-center").append(body) );
+
+    let footer = $("<button>").addClass("btn btn-primary")
+                  .click( function () { Creer_domaine ( element.domain_uuid ); } )
+                  .text("Ajouter");
+    card.append ( $("<div>").addClass("card-footer text-center").append( footer ) );
+    $("#idCardContainer").append(card);
+
     Token.grants.forEach ( function (element, index)
      { console.debug ( element );
 
@@ -9,9 +25,7 @@
        Send_to_API ( 'POST', "/domain/image", json_request, function (Response)
         { let card = $("<div>").addClass("card shadow m-1 bg-light");
 
-          let header = $("<h4>")
-                       .click( function () { Changer_domaine ( element.domain_uuid ); } )
-                       .text( element.description );
+          let header = $("<h4>").text( element.description );
           card.append ( $("<div>").addClass("card-header text-center").append(header) );
 
           let body = $("<img>").css("cursor","pointer").addClass("wtd-img-card")
@@ -20,15 +34,16 @@
                          else body.attr ("src", "https://static.abls-habitat.fr/img/syn_accueil.png" );
           card.append ( $("<div>").addClass("card-body text-center").append(body) );
 
-          let config = $("<button>").addClass("btn btn-primary")
+          let footer = $("<button>").addClass("btn btn-primary")
                          .click( function () { Redirect("/domain/"+ element.domain_uuid ); } )
                          .text("Configurer");
 
-          card.append ( $("<div>").addClass("card-footer text-center").append( config ) );
+          card.append ( $("<div>").addClass("card-footer text-center").append( footer ) );
 
-          $("#idCardContainer").append(card);
+          card.insertBefore("#idpluscard");
         });
      });
+
   }
 /******************************************************************************************************************************/
  function Changer_domaine ( domain_uuid )
