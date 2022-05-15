132d872fdc98ed58712d62b0efecbe6ca974171e
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-05-15 21:47:16 +0200

    ARCHIVE: Prepare delete feature

--- public/js/archive.js
+++ public/js/archive.js
@@ -1,31 +1,30 @@
 /************************************ Envoi les infos de modifications synoptique *********************************************/
  function Valider_Archive_Del ( table_name )
-  { var json_request = JSON.stringify( { table_name : table_name } );
-    Send_to_API ( 'DELETE', "/api/archive/del", json_request, function ()
-     { $('#idTableArchive').DataTable().ajax.reload(null, false);
+  { var json_request = { table_name : table_name };
+    Send_to_API ( 'DELETE', "/archive/del", json_request, function ()
+     { Show_toast_ok ( "Table "+table_name+" supprimée." );
+       $('#idTableArchive').DataTable().row("#"+table_name).delete();
      });
   }
 
 /********************************************* Afichage du modal d'edition synoptique *****************************************/
  function Show_Modal_Archive_Del ( table_name )
-  { table = $('#idTableArchive').DataTable();
-    selection = table.ajax.json().tables.filter( function(item) { return item.table_name==table_name } )[0];
+  { selection = $('#idTableArchive').DataTable().row("#"+table_name).data();
     $('#idModalDelTitre').text ( "Détruire la table ?" );
     $('#idModalDelMessage').html("Etes-vous sur de vouloir supprimer cette table et ses enregistrements ?"+
                                  "<hr>"+
-                                 "<strong>"+table_name + "<br>"+selection.table_rows+" enregistrements</strong>"
+                                 "<strong>"+selection.table_name + "<br>"+selection.table_rows+" enregistrements</strong>"
                                 );
-    $('#idModalDelValider').attr( "onclick", "Valider_Archive_Del('"+table_name+"')" );
+    $('#idModalDelValider').off("click").on("click", function () { Valider_Archive_Del(table_name); } );
     $('#idModalDel').modal("show");
   }
 /************************************ Envoi les infos de modifications synoptique *********************************************/
  function Archive_sauver_parametre ( )
   { var json_request =
-     { archive_retention : parseInt($("#idDomainArchRetention").val()),
+     { archive_retention : parseInt($("#idArchiveDBRetention").val()),
      };
     Send_to_API ( 'POST', "/archive/set", json_request, function(Response)
-     { Load_page();
-       Show_toast_ok ( "Modifications sauvegardées." );
+     { Show_toast_ok ( "Modifications sauvegardées." );
      }, null );
   }
 /********************************************* Appelé au chargement de la page ************************************************/
@@ -37,7 +36,7 @@
        $('#idTableArchive').DataTable(
           { pageLength : 50,
             fixedHeader: true,
-            rowId: "id",
+            rowId: "table_name",
             data:Response.tables,
             columns:
              [ { "data": "table_name", "title":"Nom de la table", "className": "align-middle text-center" },
