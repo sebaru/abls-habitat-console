bdb9ce3802af8e14e6e981a084e45d83d8d3b398
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-10-09 18:00:31 +0200

    DLS_SOURCE: Fix idErrorLog class after compil

--- public/js/dls_source.js
+++ public/js/dls_source.js
@@ -29,9 +29,9 @@
     Send_to_API ( "POST", "/dls/compil", json_request, function(Response)
      { $("#idErrorLog").html(Response.errorlog.replace(/(?:\r\n|\r|\n)/g, '<br>'));
        $("#idErrorLog").removeClass("alert-info alert-warning alert-danger alert-success");
-            if (Response.compil_status==2) { $("#idErrorLog").addClass("alert-success"); }                              /* OK */
-       else if (Response.compil_status==1) { $("#idErrorLog").addClass("alert-warning"); }                         /* Warning */
-       else $("#idErrorLog").addClass("alert-danger");
+            if (Response.error_count)   { $("#idErrorLog").addClass("alert-danger"); }                               /* Error */
+       else if (Response.warning_count) { $("#idErrorLog").addClass("alert-warning"); }                                 /* OK */
+       else $("#idErrorLog").addClass("alert-success").html("No error");
      }, null );
   }
 /********************************************* Appelé au chargement de la page ************************************************/
@@ -49,9 +49,9 @@
        $("#idSourceSynoptique").text(Response.page);
        SourceCode.getDoc().setValue(Response.sourcecode);
        $("#idErrorLog").html(Response.errorlog.replace(/(?:\r\n|\r|\n)/g, '<br>'));
-            if (Response.compil_status==2) { $("#idErrorLog").addClass("alert-success"); }                              /* OK */
-       else if (Response.compil_status==1) { $("#idErrorLog").addClass("alert-warning"); }                         /* Warning */
-       else $("#idErrorLog").addClass("alert-danger");
+            if (Response.error_count)   { $("#idErrorLog").addClass("alert-danger"); }                        /* Error */
+       else if (Response.warning_count) { $("#idErrorLog").addClass("alert-warning"); }                          /* OK */
+       else $("#idErrorLog").addClass("alert-success").html("No error");
      }, null);
   }
 /*----------------------------------------------------------------------------------------------------------------------------*/
