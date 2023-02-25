374b39c759fc4e8effebac8d1d3544ee59c0d81a
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2023-02-25 17:39:09 +0100

    FOOTER: Fix type

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -3,7 +3,7 @@
 <footer class="page-footer bg-info mt-2 pt-0"> <!-- fixed-bottom-->
 
   <!-- Copyright -->
-  <div class="footer-copyright text-right py-2">
+  <div class="footer-copyright text-right p-2">
     <small>
 
       <span class="text-secondary"><?php echo getenv("ABLS_API"); ?> -</span>
@@ -21,7 +21,7 @@
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js" ></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
