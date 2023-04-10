d14f5bd32665729bfe0cd190bbea9e67e97050d8
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2023-04-10 15:40:43 +0200

    COMMAND_TEXT: Add Delete button

--- public/js/command_text.js
+++ public/js/command_text.js
@@ -1,6 +1,16 @@
  function COMMAND_TEXT_Refresh ( )
   { $('#idTableTXT').DataTable().ajax.reload(null, false);
   }
+/**************************************** Supprime une connexion modbus *******************************************************/
+ function COMMAND_TEXT_Del (mapping_id)
+  { selection = $('#idTableTXT').DataTable().row("#"+mapping_id).data();
+    Show_modal_del ( "Supprimer le mapping "+selection.thread_acronyme,
+                     "Etes-vous sûr de vouloir supprimer ce mapping ?",
+                     selection.thread_acronyme + " - "+selection.tech_id +":"+ selection.acronyme,
+                     function () { Send_to_API ( 'DELETE', "/mapping/delete", { mapping_id: parseInt(mapping_id) },
+                                                 function () { COMMAND_TEXT_Refresh (); } )
+                                 });
+  }
 /********************************************* Afichage du modal d'edition synoptique *****************************************/
  function COMMAND_TEXT_Add ( )
   { var json_request = { thread_tech_id: "_COMMAND_TEXT", thread_acronyme: $('#idModalCommandTextAdd').val().toUpperCase(),
@@ -44,13 +54,10 @@
                    } else return( "--" );
                 }
             },
-            { "data": null, "title":"Description", "className": "align-middle text-center",
-              "render": function (item)
-                { if(item.tech_id) { return ( item.libelle ); } else return( "--" ); }
-            },
             { "data": null, "title":"Actions", "orderable": false, "render": function (item)
                 { boutons = Bouton_actions_start ();
                   boutons += Bouton_actions_add ( "primary", "Mapper cette commande", "COMMAND_TEXT_Map_DI", item.mapping_id, "directions", null );
+                  boutons += Bouton_actions_add ( "danger", "Supprimer le mapping", "COMMAND_TEXT_Del", item.mapping_id, "trash", null );
                   boutons += Bouton_actions_end ();
                   return(boutons);
                 },
