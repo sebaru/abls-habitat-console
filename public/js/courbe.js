af8584eddfb6c45da85647b50948f5c702c6b54f
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2023-07-11 20:30:33 +0200

    COURBE: Fix SetPeriod

--- public/js/courbe.js
+++ public/js/courbe.js
@@ -1,10 +1,6 @@
-/********************************************* Changement de période **********************************************************/
- function Courbe_Set_Period ()
-  { vars = window.location.pathname.split('/');
-    Redirect ( "/courbe/"+vars[2]+"/"+vars[3]+"/"+$("#idCourbePeriod").val() );
-  }
-/********************************************* Chargement d(une courbe ********************************************************/
+/********************************************* Chargement d'une courbe ********************************************************/
  function Load_page ()
   { vars = window.location.pathname.split('/');
+    $("#idCourbePeriod").off("change").on("change", function () { Redirect ( "/courbe/"+vars[2]+"/"+vars[3]+"/"+$("#idCourbePeriod").val() ); } );
     Charger_une_courbe ( "idChartCourbe", vars[2], vars[3], vars[4] )
   }
