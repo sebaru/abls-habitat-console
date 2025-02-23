faaba5043fc8f30cd6e7be5aa9ebc178c3dd3801
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2025-02-23 19:24:55 +0100

    DLS_SOURCE: Add line number control before saving

--- public/js/dls_source.js
+++ public/js/dls_source.js
@@ -1,30 +1,23 @@
-
+ var Dls;
  var SourceCode;
 
  function Go_to_messages ()
   { vars = window.location.pathname.split('/');
-    Redirect ( "/messages/"+vars[2] );
+    Redirect ( "/messages/"+Dls.tech_id );
   }
  function Go_to_source ()
-  { vars = window.location.pathname.split('/');
-    Redirect ( "/dls/"+vars[2] );
+  { Redirect ( "/dls/"+Dls.tech_id );
   }
  function Go_to_dls_run ()
-  { vars = window.location.pathname.split('/');
-    Redirect ( "/dls/run/"+vars[2] );
+  { Redirect ( "/dls/run/"+Dls.tech_id );
   }
 /********************************************* Appelé au chargement de la page ************************************************/
- function Compiler ()
-  { vars = window.location.pathname.split('/');
-    var json_request =
-     { tech_id : vars[2],
+ function Compiler_valider ()
+  { var json_request =
+     { tech_id : Dls.tech_id,
        sourcecode: SourceCode.getDoc().getValue(),
      };
-
-    if (json_request.sourcecode.length == 0)
-     { Show_Error ( "Un plugin vide n'est pas autorisé" );
-       return;
-     }
+    Dls.nbr_ligne = SourceCode.lineCount();
 
     Send_to_API ( "POST", "/dls/compil", json_request, function(Response)
      { $("#idErrorLog").html(Response.errorlog.replace(/(?:\r\n|\r|\n)/g, '<br>'));
@@ -33,6 +26,26 @@
        else if (Response.warning_count) { $("#idErrorLog").addClass("alert-warning"); }                                 /* OK */
        else $("#idErrorLog").addClass("alert-success").html("No error");
      }, null );
+  }
+/********************************************* Appelé au chargement de la page ************************************************/
+ function Compiler ()
+  { if (SourceCode.getDoc().getValue().length == 0)
+     { Show_Error ( "Un plugin vide n'est pas autorisé" );
+       return;
+     }
+
+    if (Dls.nbr_ligne > 50)
+     { var min = Dls.nbr_ligne * 0.8;
+       var max = Dls.nbr_ligne * 1.2;
+       if (SourceCode.lineCount() < min || SourceCode.lineCount() > max)
+        { Show_modal_del ( "Confirmation de compilation",
+                           "Beaucoup de lignes ont été modifiées. Etes-vous sûr d'appliquer ces changements ?",
+                           "Nombre de lignes avant édition: "+ Dls.nbr_ligne +". Après édition: " + SourceCode.lineCount() + ".",
+                           function () { Compiler_valider(); } );
+          return;
+        }
+     }
+    else Compiler_valider ();
   }
 /********************************************* Appelé au chargement de la page ************************************************/
  function Load_page ()
@@ -50,6 +63,7 @@
             if (Response.error_count)   { $("#idErrorLog").addClass("alert-danger"); }                        /* Error */
        else if (Response.warning_count) { $("#idErrorLog").addClass("alert-warning"); }                          /* OK */
        else $("#idErrorLog").addClass("alert-success").html("No error");
+       Dls = Response; /* Sauvegarde de la reponse */
      }, null);
   }
 /*----------------------------------------------------------------------------------------------------------------------------*/
