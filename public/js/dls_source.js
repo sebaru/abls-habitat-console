16f765bf4f682b4787ac0695bbf9c9750d1f57a9
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2025-04-23 19:04:10 +0200

    DLS: Separate Compil and Restart

--- public/js/dls_source.js
+++ public/js/dls_source.js
@@ -33,8 +33,7 @@
        $("#idErrorLog").removeClass("alert-info alert-warning alert-danger alert-success");
             if (Response.error_count)   { $("#idErrorLog").addClass("alert-danger"); }                               /* Error */
        else if (Response.warning_count) { $("#idErrorLog").addClass("alert-warning"); }                                 /* OK */
-       else { $("#idErrorLog").addClass("alert-success").html("No error"); Dls_Restart(); }
-
+       else { $("#idErrorLog").addClass("alert-success").html("No error"); }
      }, null );
   }
 /********************************************* Appelé au chargement de la page ************************************************/
