1a042640fdea75f374781cdcfd7312d06bd9f6df
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2025-05-01 18:42:38 +0200

    DASHBOARD: Add NBR_CLEANUP

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -167,6 +167,10 @@
   </div>
 
   <div class="row p-1 justify-content-center">
+   <canvas id="idCourbeNbCleanup" class="courbe-dashboard border border-info"></canvas>
+  </div>
+
+  <div class="row p-1 justify-content-center">
    <canvas id="idCourbeDlsMaxRss" class="courbe-dashboard border border-info"></canvas>
   </div>
 
